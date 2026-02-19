0944cf93f6224b76837217e6d1f851c163886551
sabaka-chabaka <gamekimg360@gmail.com>
2026-02-19 14:43:04 +0400

    Base CLI features

--- SabakaLang.CLI/Program.cs
+++ SabakaLang.CLI/Program.cs
@@ -1,3 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SabakaLang.CLI.Project;
 
-Console.WriteLine("Hello, World!");
+namespace SabakaLang.CLI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        switch (args[0])
+        {
+            case "run":
+            {
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("Usage: sabaka run [project-name]");
+                    return;
+                }
+
+                ProjectManager.Run(args[1]);
+                break;
+            }
+
+            case "new":
+            {
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("Usage: sabaka run [project-name]");
+                    return;
+                }
+
+                if (Directory.Exists(args[1]))
+                {
+                    Console.WriteLine("Directory already exists");
+                    return;
+                }
+                
+                ProjectManager.NewProject(args[1]);
+                break;
+            }
+
+            case "build":
+            {
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("Usage: sabaka build [project-name]");
+                    return;
+                }
+
+                if (!Directory.Exists(args[1]))
+                {
+                    Console.WriteLine("No project directory exists");
+                    return;
+                }
+                
+                ProjectManager.Build(args[1]);
+                break;
+            }
+            
+            default:
+                Console.WriteLine("Usage: sabaka [command]");
+                break;
+        }
+    }
+}
---
+++ SabakaLang.CLI/Project/Project.cs
@@ -0,0 +1,7 @@
+namespace SabakaLang.CLI.Project;
+
+public class Project
+{
+    public string Name { get; set; }
+    public string Entry { get; set; }
+}
