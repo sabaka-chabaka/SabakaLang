c4f157cec753529c750361cdfcf96eb6595476c4
sabaka-chabaka <gamekimg360@gmail.com>
2026-04-11 16:12:08 +0400

    ArrayTests.cs && FunctionTests.cs

---
+++ SabakaLang.Runtime.UnitTests/ArrayTests.cs
@@ -0,0 +1,121 @@
+namespace SabakaLang.Runtime.UnitTests;
+
+public class ArrayTests : Utilities
+{
+    [Fact]
+    public void ArrayLiteral_CreatesCorrectly()
+        => Assert.Equal("3", Output("int[] a = [1, 2, 3]; print(a.length);"));
+ 
+    [Fact]
+    public void EmptyArray_HasZeroLength()
+        => Assert.Equal("0", Output("int[] a = []; print(a.length);"));
+    
+    [Fact]
+    public void ArrayLoad_ReadsCorrectElement()
+        => Assert.Equal("20", Output("int[] a = [10, 20, 30]; print(a[1]);"));
+ 
+    [Fact]
+    public void ArrayStore_UpdatesElement()
+        => Assert.Equal("99", Output("int[] a = [1, 2, 3]; a[2] = 99; print(a[2]);"));
+ 
+    [Fact]
+    public void ArrayStore_GrowsArray_WhenIndexBeyondSize()
+    {
+        var src = """
+                  int[] a = [0];
+                  a[5] = 42;
+                  print(a[5]);
+                  """;
+        Assert.Equal("42", Output(src));
+    }
+ 
+    [Fact]
+    public void ArrayLoad_OutOfBounds_Throws()
+        => RunError("int[] a = [1, 2]; print(a[5]);", "out of bounds");
+ 
+    [Fact]
+    public void ArrayLoad_NegativeIndex_Throws()
+        => RunError("int[] a = [1]; print(a[-1]);", "out of bounds");
+    
+    [Fact]
+    public void StringIndex_ReturnsCharacter()
+        => Assert.Equal("e", Output("string s = \"hello\"; print(s[1]);"));
+ 
+    [Fact]
+    public void StringLength_ReturnsCount()
+        => Assert.Equal("5", Output("print(\"hello\".length);"));
+ 
+    [Fact]
+    public void StringIndex_OutOfBounds_Throws()
+        => RunError("string s = \"hi\"; print(s[10]);", "out of range");
+    
+    [Fact]
+    public void ForLoop_IteratesArray()
+    {
+        var src = """
+                  int[] a = [1, 2, 3, 4, 5];
+                  int s = 0;
+                  for (int i = 0; i < a.length; i = i + 1) {
+                      s = s + a[i];
+                  }
+                  print(s);
+                  """;
+        Assert.Equal("15", Output(src));
+    }
+ 
+    [Fact]
+    public void Foreach_CollectsStrings()
+    {
+        var src = """
+                  string[] words = ["foo", "bar", "baz"];
+                  string result = "";
+                  foreach (string w in words) {
+                      result = result + w;
+                  }
+                  print(result);
+                  """;
+        Assert.Equal("foobarbaz", Output(src));
+    }
+    
+    [Fact]
+    public void NestedArrays_AccessElement()
+    {
+        var src = """
+                  int[] inner = [10, 20];
+                  int[][] outer = [inner];
+                  print(outer[0][1]);
+                  """;
+        Assert.Equal("20", Output(src));
+    }
+    
+    [Fact]
+    public void Function_ReturnsArray()
+    {
+        var src = """
+                  int[] make() {
+                      int[] a = [7, 8, 9];
+                      return a;
+                  }
+                  int[] r = make();
+                  print(r[0]);
+                  print(r[2]);
+                  """;
+        Assert.Equal(["7", "9"], Lines(src));
+    }
+    
+    [Fact]
+    public void Ord_ReturnsAsciiCode()
+        => Assert.Equal("65", Output("print(ord(\"A\"));"));
+ 
+    [Fact]
+    public void Chr_ReturnsCharacter()
+        => Assert.Equal("Z", Output("print(chr(90));"));
+ 
+    [Fact]
+    public void OrdChr_RoundTrip()
+        => Assert.Equal("K", Output("print(chr(ord(\"K\")));"));
+ 
+    [Fact]
+    public void Ord_EmptyString_Throws()
+        => RunError("ord(\"\");", "empty string");
+}
---
+++ SabakaLang.Runtime.UnitTests/FunctionTests.cs
@@ -0,0 +1,202 @@
+using SabakaLang.Compiler;
+
+namespace SabakaLang.Runtime.UnitTests;
+
+public sealed class FunctionTests : Utilities
+{
+    [Fact]
+    public void Function_VoidCallExecutesBody()
+    {
+        var src = """
+            void greet() { print("hi"); }
+            greet();
+            """;
+        Assert.Equal("hi", Output(src));
+    }
+
+    [Fact]
+    public void Function_ReturnsValue()
+    {
+        var src = """
+            int double(int x) { return x * 2; }
+            print(double(21));
+            """;
+        Assert.Equal("42", Output(src));
+    }
+
+    [Fact]
+    public void Function_MultipleParams()
+    {
+        var src = """
+            int add(int a, int b) { return a + b; }
+            print(add(3, 4));
+            """;
+        Assert.Equal("7", Output(src));
+    }
+
+    [Fact]
+    public void Function_ReturnEarlyFromIf()
+    {
+        var src = """
+            string sign(int n) {
+                if (n > 0) { return "pos"; }
+                if (n < 0) { return "neg"; }
+                return "zero";
+            }
+            print(sign(-5));
+            print(sign(0));
+            print(sign(7));
+            """;
+        Assert.Equal(["neg", "zero", "pos"], Lines(src));
+    }
+
+    [Fact]
+    public void Function_LocalVarDoesNotLeakToOuter()
+    {
+        var src = """
+            void foo() { int secret = 99; }
+            foo();
+            """;
+        RunError("""
+            void foo() { int secret = 99; }
+            foo();
+            print(secret);
+            """, "undefined variable");
+    }
+    
+    [Fact]
+    public void Recursion_Factorial()
+    {
+        var src = """
+            int fact(int n) {
+                if (n <= 1) { return 1; }
+                return n * fact(n - 1);
+            }
+            print(fact(6));
+            """;
+        Assert.Equal("720", Output(src));
+    }
+
+    [Fact]
+    public void Recursion_Fibonacci()
+    {
+        var src = """
+            int fib(int n) {
+                if (n <= 1) { return n; }
+                return fib(n - 1) + fib(n - 2);
+            }
+            print(fib(10));
+            """;
+        Assert.Equal("55", Output(src));
+    }
+
+    [Fact]
+    public void Recursion_MutualRecursion()
+    {
+        var src = """
+            bool isEven(int n) {
+                if (n == 0) { return true; }
+                return isOdd(n - 1);
+            }
+            bool isOdd(int n) {
+                if (n == 0) { return false; }
+                return isEven(n - 1);
+            }
+            print(isEven(4));
+            print(isOdd(7));
+            """;
+        Assert.Equal(["true", "true"], Lines(src));
+    }
+
+    [Fact]
+    public void Function_ArrayPassedByReference()
+    {
+        var src = """
+            void fill(int[] arr) {
+                arr[0] = 99;
+            }
+            int[] a = [0, 0, 0];
+            fill(a);
+            print(a[0]);
+            """;
+        Assert.Equal("99", Output(src));
+    }
+    
+    [Fact]
+    public void CallFunction_FromCSharp_ReturnsCorrectValue()
+    {
+        var src = """
+            int square(int n) { return n * n; }
+            """;
+
+        var instructions = CompileToInstructions(src);
+        var (vm, _) = BuildVm();
+        vm.Execute(instructions);
+
+        var result = vm.CallFunction("square", [Value.FromInt(7)]);
+        Assert.Equal(SabakaType.Int, result.Type);
+        Assert.Equal(49, result.Int);
+    }
+
+    [Fact]
+    public void CallFunction_WithStringArg_Correct()
+    {
+        var src = """
+            string greet(string name) { return "hello " + name; }
+            """;
+
+        var instructions = CompileToInstructions(src);
+        var (vm, _) = BuildVm();
+        vm.Execute(instructions);
+
+        var result = vm.CallFunction("greet", [Value.FromString("world")]);
+        Assert.Equal("hello world", result.String);
+    }
+
+    [Fact]
+    public void CallFunction_Undefined_Throws()
+    {
+        var src = "int x = 1;";
+        var instructions = CompileToInstructions(src);
+        var (vm, _) = BuildVm();
+        vm.Execute(instructions);
+
+        Assert.Throws<RuntimeException>(() =>
+            vm.CallFunction("nonExistent", []));
+    }
+    
+    [Fact]
+    public void ExternalFunction_CalledFromScript()
+    {
+        var called = false;
+        var rt = new SabakaRuntime();
+        rt.RegisterExternal("ping", 0, _ => { called = true; return Value.Null; });
+
+        rt.RunAndCapture("ping();");
+        Assert.True(called);
+    }
+
+    [Fact]
+    public void ExternalFunction_ReceivesArgs()
+    {
+        Value[]? received = null;
+        var rt = new SabakaRuntime();
+        rt.RegisterExternal("capture", 2, args => { received = args; return Value.Null; });
+
+        rt.RunAndCapture("capture(1, 2);");
+        Assert.NotNull(received);
+        Assert.Equal(2, received!.Length);
+        Assert.Equal(1, received[0].Int);
+        Assert.Equal(2, received[1].Int);
+    }
+
+    [Fact]
+    public void ExternalFunction_ReturnsValueToScript()
+    {
+        var rt = new SabakaRuntime();
+        rt.RegisterExternal("give42", 0, _ => Value.FromInt(42));
+
+        var output = rt.RunAndCapture("print(give42());");
+        Assert.Contains("42", output);
+    }
+}
