7e381cf2507c16fe09a852f2c852d7689326e464
sabaka-chabaka <gamekimg360@gmail.com>
2026-04-14 14:58:28 +0400

    sabaka run <file>

--- SabakaLang.Cli/Program.cs
+++ SabakaLang.Cli/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SabakaLang.Compiler;
+using SabakaLang.Runtime;
+
+namespace SabakaLang.Cli;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+            throw new ArgumentException("Usage: sabaka <command> <args>");
+
+        switch (args[0])
+        {
+            default:
+                throw new Exception($"Unknown command '{args[0]}'");
+                break;
+            
+            case "run":
+                var src = File.ReadAllText(args[1]);
+
+                var lexer = new Lexer(src);
+                var parser = new Parser(lexer.Tokenize());
+                var binder = new Binder();
+                var compiler = new Compiler.Compiler();
+                var result = compiler.Compile(parser.Parse().Statements, binder.Bind(parser.Parse().Statements));
+
+                var vm = new VirtualMachine();
+                vm.Execute(result.Code.ToList());
+                
+                break;
+        }
+    }
+}
