ead5a45920b6fe654a67b97a5eee382e12a1a649
sabaka-chabaka <gamekimg360@gmail.com>
2026-04-18 12:36:01 +0400

    runner completed i think

--- SabakaLang.Runner/Program.cs
+++ SabakaLang.Runner/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SabakaLang.Compiler;
+using SabakaLang.Runtime;
+
+namespace SabakaLang.Runner;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Instruction> instructions = Reader.Read(args[0]);
+        
+        VirtualMachine virtualMachine = new();
+        virtualMachine.Execute(instructions);
+    }
+}
