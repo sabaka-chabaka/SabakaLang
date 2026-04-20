924cd3bce0838aff785e1103e71631b20a16b1f9
sabaka-chabaka <gamekimg360@gmail.com>
2026-04-20 22:13:19 +0400

    fix

--- SabakaLang.Studio/Elements/EditorDrawable.cs
+++ SabakaLang.Studio/Elements/EditorDrawable.cs
@@ -2,7 +2,7 @@ namespace SabakaLang.Studio.Elements;
 
 public class EditorDrawable : IDrawable
 {
-    public EditorEngine Engine { get; set; }
+    public EditorEngine? Engine { get; set; }
 
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
@@ -17,6 +17,29 @@ public class EditorDrawable : IDrawable
 
         float lineHeight = 18f;
         float charWidth = 8.4f;
+
+        if (Engine.HasSelection)
+        {
+            var startRow = Engine.SelectionStartRow;
+            var startCol = Engine.SelectionStartCol;
+            var endRow = Engine.CursorRow;
+            var endCol = Engine.CursorCol;
+
+            if (startRow > endRow || (startRow == endRow && startCol > endCol))
+            {
+                (startRow, endRow) = (endRow, startRow);
+                (startCol, endCol) = (endCol, startCol);
+            }
+
+            canvas.FillColor = Color.FromRgba(255, 255, 255, 50);
+            for (int r = startRow; r <= endRow; r++)
+            {
+                float y = r * lineHeight + 4;
+                float xStart = (r == startRow) ? (10 + startCol * charWidth) : 10;
+                float xEnd = (r == endRow) ? (10 + endCol * charWidth) : (10 + Engine.Lines[r].Length * charWidth + 5);
+                canvas.FillRectangle(xStart, y, xEnd - xStart, lineHeight);
+            }
+        }
 
         for (int i = 0; i < Engine.Lines.Count; i++)
         {
--- SabakaLang.Studio/Logical/EditorEngine.cs
+++ SabakaLang.Studio/Logical/EditorEngine.cs
@@ -7,15 +7,28 @@ public class EditorEngine
     public List<StringBuilder> Lines = new() { new StringBuilder() };
     public int CursorRow = 0;
     public int CursorCol = 0;
+    
+    public int SelectionStartRow = -1;
+    public int SelectionStartCol = -1;
+
+    public bool HasSelection => SelectionStartRow != -1;
+
+    public void ClearSelection()
+    {
+        SelectionStartRow = -1;
+        SelectionStartCol = -1;
+    }
 
     public void InsertChar(char c)
     {
+        if (HasSelection) DeleteSelection();
         Lines[CursorRow].Insert(CursorCol, c);
         CursorCol++;
     }
     
     public void InsertNewLine()
     {
+        if (HasSelection) DeleteSelection();
         var remainder = Lines[CursorRow].ToString().Substring(CursorCol);
         Lines[CursorRow].Remove(CursorCol, remainder.Length);
         Lines.Insert(CursorRow + 1, new StringBuilder(remainder));
@@ -25,6 +38,12 @@ public class EditorEngine
     
     public void Backspace()
     {
+        if (HasSelection)
+        {
+            DeleteSelection();
+            return;
+        }
+
         if (CursorCol > 0)
         {
             Lines[CursorRow].Remove(CursorCol - 1, 1);
@@ -37,5 +56,40 @@ public class EditorEngine
             Lines.RemoveAt(CursorRow);
             CursorRow--;
         }
+    }
+
+    public void DeleteSelection()
+    {
+        if (!HasSelection) return;
+
+        var startRow = SelectionStartRow;
+        var startCol = SelectionStartCol;
+        var endRow = CursorRow;
+        var endCol = CursorCol;
+
+        if (startRow > endRow || (startRow == endRow && startCol > endCol))
+        {
+            (startRow, endRow) = (endRow, startRow);
+            (startCol, endCol) = (endCol, startCol);
+        }
+
+        if (startRow == endRow)
+        {
+            Lines[startRow].Remove(startCol, endCol - startCol);
+        }
+        else
+        {
+            Lines[startRow].Remove(startCol, Lines[startRow].Length - startCol);
+            Lines[startRow].Append(Lines[endRow].ToString().Substring(endCol));
+            
+            for (int i = 0; i < endRow - startRow; i++)
+            {
+                Lines.RemoveAt(startRow + 1);
+            }
+        }
+
+        CursorRow = startRow;
+        CursorCol = startCol;
+        ClearSelection();
     }
 }
