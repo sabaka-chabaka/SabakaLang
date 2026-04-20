e2cf7112fc957d73b40065688d035acf29d4eb16
sabaka-chabaka <gamekimg360@gmail.com>
2026-04-20 21:44:46 +0400

    Updates

--- SabakaLang.Studio/MauiProgram.cs
+++ SabakaLang.Studio/MauiProgram.cs
@@ -11,8 +11,7 @@ public static class MauiProgram
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("JetBrainsMono-Regular.ttf", "JetMonoRegular");
             });
 
 #if DEBUG
