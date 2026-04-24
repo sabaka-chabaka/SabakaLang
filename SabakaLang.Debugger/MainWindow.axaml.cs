eb688989dc20af872099a9b26dac5990a0b84965
sabaka-chabaka <gamekimg360@gmail.com>
2026-04-24 21:16:35 +0400

    minimal debugger

--- SabakaLang.Debugger/MainWindow.axaml.cs
+++ SabakaLang.Debugger/MainWindow.axaml.cs
@@ -1,11 +1,74 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using SabakaLang.Compiler;
 
 namespace SabakaLang.Debugger;
 
 public partial class MainWindow : Window
 {
+    private List<Instruction> _bytecode = new();
+    private int _ip = 0;
+
     public MainWindow()
     {
         InitializeComponent();
+        StepButton.Click += StepButton_OnClick;
+    }
+
+    private async void DebugButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(PathBlock.Text))
+        {
+            var files = await StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions {
+                Title = "Open SabakaSource",
+                AllowMultiple = false
+            });
+
+            if (files.Count >= 1) PathBlock.Text = files[0].Path.LocalPath;
+            else return;
+        }
+
+        try 
+        {
+            var source = await File.ReadAllTextAsync(PathBlock.Text);
+            var lex = new Lexer(source).Tokenize();
+            var parser = new Parser(lex).Parse();
+            var bind = new Binder().Bind(parser.Statements);
+            var comp = new Compiler.Compiler();
+            
+            _bytecode = comp.Compile(parser.Statements, bind).Code.ToList(); 
+            
+            _ip = 0;
+            ConsoleOutput.Text = "Compilation successful. Ready to debug.\n";
+            UpdateCodeDisplay();
+        }
+        catch (Exception ex)
+        {
+            ConsoleOutput.Text = $"Error: {ex.Message}";
+        }
+    }
+
+    private void StepButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (_bytecode == null || _ip >= _bytecode.Count) return;
+
+        var current = _bytecode[_ip];
+
+        ConsoleOutput.Text += $"[{_ip}] Executed: {current}\n";
+        _ip++;
+        UpdateCodeDisplay();
+    }
+
+    private void UpdateCodeDisplay()
+    {
+        var lines = _bytecode.Select((inst, i) => 
+            (i == _ip ? " > " : "   ") + $"[{i:D3}] {inst.ToString()}");
+        
+        CodeArea.Text = string.Join(Environment.NewLine, lines);
     }
 }
